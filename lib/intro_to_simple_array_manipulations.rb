1cb84b03ff020b580e67d07433ba767c6f828a5c
Scott Gale <galejscott@gmail.com>
2019-05-06 21:03:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,45 +3,6 @@ def using_push(array, element)
 end
 
 def using_unshift(array, element)
-  array.unshift
+  array.unshift(element)
 end
 
-def using_pop
-  
-end
-
-def pop_with_args
-  
-end
-
-def using_shift
-  
-end
-
-def shift_with_args
-  
-end
-
-def using_concat
-  
-end
-
-def using_insert
-  
-end
-
-def using_uniq
-  
-end
-
-def using_flatten
-  
-end
-
-def using_delete
-  
-end
-
-def using_delete_at
-  
-end
